e208a4200ba55b06aaee8c052dfac45855b08551
Shashank Mishra <shashank.mishra.dev14@gmail.com>
2024-12-29 21:52:32 +0530

    [Feature] : [Added Navbar and changing the entire structure of the website]

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+		extend: {
+			fontFamily: {
+			  sans: ["'Satoshi'", 'sans-serif'],
+			},
+		  },
+		},
+		plugins: [],
+	  };
